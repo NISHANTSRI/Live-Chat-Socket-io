c690bab22809c63bd79b44e39dcdeb54c6adad72
Nishant Srivastav <nishantsri18@gmail.com>
2022-08-14 14:08:49 +0530

    near end commit

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 function Chat({ socket, name, room }) {
 
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
 
     const sendMessage = async () => {
         if (currentMessage !== "") {
@@ -16,13 +17,14 @@ function Chat({ socket, name, room }) {
             };
 
             await socket.emit('send_message', messageData);
+            setMessageList((list) => [...list, messageData])
         }
     }
 
     // this use effect hook was created because after we have received the messsage from our own what we want to do is to let ohet also send and recieve our mesaage ,,, so what we did is we crete anoter socket event receive message in the use effect hook and gave it the data that was recieved in the index.js vis the send_message event to asychrunosly.
     useEffect(() => {
         socket.on('receive_message', (data) => {
-            console.log(data);
+            setMessageList((list) => [...list, data])
         })
     }, [socket])
 
@@ -32,11 +34,29 @@ function Chat({ socket, name, room }) {
             <div className="chat-header">
                 <p> Live Chat </p>
             </div>
-            <div className="chat-body"></div>
+            <div className="chat-body">
+                {messageList.map((messageContent) => {
+                    return <div className="message" id={name === messageContent.author ? 'you' : 'other'}>
+                        <div>
+                            <div className="message-content">
+                                <p>{messageContent.message}</p>
+                            </div>
+                            <div className="message-meta">
+                                <p id='time'>{messageContent.time}</p>
+                                <p id='author'>{messageContent.author}</p>
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
             <div className="chat-footer">
                 <input type='text' placeholder='Hey!'
                     onChange={(e) => {
                         setCurrentMessage(e.target.value)
+                    }}
+
+                    onKeyPress={(e) => {
+                        e.key === 'Enter' && sendMessage();
                     }}
                 />
                 <button onClick={sendMessage}>&#9658;</button>
